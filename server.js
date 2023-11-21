a5534144b777f7d8194caeaf2eaa6c38325237a7
Joseph <jtawil2015@gmail.com>
2023-11-21 12:49:57 -0800

    fix"

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 
 const app = express();
-const port = 3000;
+const port = 5001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
